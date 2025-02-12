dd309299373a3a7d797279770608ddbec796b0cb
AndreiSolomenko <andrsolo10@gmail.com>
2025-02-12 10:22:53 +0200

    js change

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-document.getElementById('jsonDropdown').addEventListener('change', function() {
-    const fileName = this.value; // Отримуємо ім'я вибраного файлу
+function loadJSON(fileName) {
     fetch(fileName)
-        .then(response => response.json())  // Завантажуємо JSON
+        .then(response => response.json())
         .then(data => {
-            // Наповнюємо текстом контейнер
             document.getElementById('textContainer').innerHTML = data.text;
         })
         .catch(error => console.error('Error loading the JSON file:', error));
+}
+
+// Завантажуємо перший файл при відкритті сторінки
+document.addEventListener("DOMContentLoaded", function() {
+    const defaultFile = document.getElementById('jsonDropdown').value;
+    loadJSON(defaultFile);
+});
+
+// Обробник подій для зміни селекта
+document.getElementById('jsonDropdown').addEventListener('change', function() {
+    loadJSON(this.value);
 });
